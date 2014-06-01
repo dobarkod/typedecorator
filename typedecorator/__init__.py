53af01aec8692ea517d044cc559d558b26e77314
Senko Rasic <senko.rasic@dobarkod.hr>
2014-06-01 23:30:54 +0200

    bump version to 0.0.3

--- typedecorator/__init__.py
+++ typedecorator/__init__.py
@@ -79,7 +79,7 @@ as `dict` and `{object}` is the same as  `set`.
 
 """
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 __all__ = ['returns', 'void', 'params', 'setup_typecheck', 'Union',
     'Nullable', 'typed']
